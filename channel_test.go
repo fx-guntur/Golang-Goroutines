48005542e7fbf39bca5651af318656d704efd678
guntur <fxguntur04@gmail.com>
2025-05-08 23:22:57 +0700

    add: Test Channel In and Out

--- channel_test.go
+++ channel_test.go
@@ -40,3 +40,24 @@ func TestChannelAsParameter(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Mega Bobi"
+}
+
+func OnlyOut(channel <-chan string) {
+	time.Sleep(2 * time.Second)
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
