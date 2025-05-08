0914cb9442d6b9b42ad6c89a13c15af99782dd1e
guntur <fxguntur04@gmail.com>
2025-05-08 21:43:17 +0700

    add: Test Many Goroutine

--- goroutine_test.go
+++ goroutine_test.go
@@ -10,9 +10,21 @@ func RunHelloWorld() {
 	fmt.Println("Hello world")
 }
 
-func TestCreateGoRoutine(t *testing.T) {
+func DisplayNumber(number int) {
+	fmt.Println(number)
+}
+
+func TestCreateGoroutine(t *testing.T) {
 	go RunHelloWorld()
 	fmt.Println("Upss!")
 
 	time.Sleep(1 * time.Second)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(5 * time.Second)
 }
